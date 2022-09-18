a885506bb891ffc9d3a4ec029cf83913c3941ec0
nokazn <41154684+nokazn@users.noreply.github.com>
2022-09-18 21:56:57 +0900

    feat: add `rotateTable` function

--- format.go
+++ format.go
@@ -14,3 +14,21 @@ func getMaxLength(row Row, min int) int {
 	}
 	return max
 }
+
+func rotateTable(table Table) Table {
+	if len(table) == 0 {
+		return []Row{}
+	}
+	firstRow := table[0]
+	if len(firstRow) == 0 {
+		return []Row{}
+	}
+	rotatedTable := make(Table, len(firstRow))
+	for x := range firstRow {
+		rotatedTable[x] = make(Row, len(table))
+		for y := range table {
+			rotatedTable[x][y] = table[y][x]
+		}
+	}
+	return rotatedTable
+}
--- format_test.go
+++ format_test.go
@@ -12,7 +12,6 @@ func TestGetMaxLength(t *testing.T) {
 		row Row
 		min int
 	}
-
 	cases := []utils.TestCase[Input, int]{
 		{
 			Input:    Input{Row{"", ""}, -1},
@@ -35,5 +34,67 @@ func TestGetMaxLength(t *testing.T) {
 			res := getMaxLength(input.row, input.min)
 			ok := reflect.DeepEqual(res, expected)
 			return res, ok
-		}})
+		},
+	})
+}
+
+func TestRotateMatrix(t *testing.T) {
+	cases := []utils.TestCase[Table, Table]{
+		{
+			Input: Table{
+				Row{"11", "21", "31", "41"},
+			},
+			Expected: Table{
+				Row{"11"},
+				Row{"21"},
+				Row{"31"},
+				Row{"41"},
+			},
+		},
+		{
+			Input: Table{
+				Row{"11", "21", "31"},
+				Row{"12", "22", "32"},
+				Row{"13", "23", "33"},
+			},
+			Expected: Table{
+				Row{"11", "12", "13"},
+				Row{"21", "22", "23"},
+				Row{"31", "32", "33"},
+			},
+		},
+		{
+			Input: Table{
+				Row{"11", "21", "31", "41"},
+				Row{"12", "22", "32", "42"},
+				Row{"13", "23", "33", "43"},
+			},
+			Expected: Table{
+				Row{"11", "12", "13"},
+				Row{"21", "22", "23"},
+				Row{"31", "32", "33"},
+				Row{"41", "42", "43"},
+			},
+		},
+		{
+			Input:    Table{},
+			Expected: Table{},
+		},
+		{
+			Input: Table{
+				Row{},
+			},
+			Expected: Table{},
+		},
+	}
+
+	utils.RunTests(t, utils.TestRunner[Table, Table]{
+		Fn:    rotateTable,
+		Cases: cases,
+		Runner: func(input Table, expected Table) (Table, bool) {
+			res := rotateTable(input)
+			ok := reflect.DeepEqual(res, expected)
+			return res, ok
+		},
+	})
 }
